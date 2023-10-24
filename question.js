a97cdf51d7a6a5f9e841edfdc4c03f213fa9d0a0
neelamtush <146577215+neelamtush@users.noreply.github.com>
2023-10-24 23:21:53 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,94 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "Entomology is the science that studies?",
+      answer: "Insects",
+      options: [
+        "Behavior of human beings",
+        "Insects",
+        "The origin and history of technical and scientific terms",
+        "The formation of rocks"
+      ]
+    },
+      {
+      numb: 2,
+      question: "For which of the following disciplines is Nobel Prize awarded?",
+      answer: "All of the above",
+      options: [
+        "Physics and Chemistry",
+        "Physics or Medicine",
+        "Literature,Peace and Economice",
+        "All of the above"
+      ]
+    },
+    {
+      numb: 3,
+      question: "Golf player Vijay Singh belongs to which country?",
+      answer: "Fiji",
+      options: [
+        "USA",
+        "India",
+        "Fiji",
+        "UK"
+      ]
+    },
+      {
+      numb: 4,
+      question: "What does CSS stand for?",
+      answer: "Cascading Style Sheet",
+      options: [
+        "Common Style Sheet",
+        "Colorful Style Sheet",
+        "Computer Style Sheet",
+        "Cascading Style Sheet"
+      ]
+    },
+    {
+        numb: 5,
+        question: "Golf player Vijay Singh belongs to which country?",
+        answer: "Winston Churchill",
+        options: [
+          "Clement Attlee",
+          "Winston Churchill",
+          "Anthony Eden",
+          "Harold Wilson"
+        ]
+      } ,
+      {
+        numb: 6,
+        question: "What is the highest governing body of basketball?",
+        answer: "FIBA",
+        options: [
+          "EuroLeague",
+          "NBA",
+          "FIBA",
+          "UBA"
+        ]
+      } ,
+      {
+        numb: 7,
+        question: "When was basketball made?",
+        answer: "1891",
+        options: [
+          "1991",
+          "1891",
+          "1978",
+          "1935"
+        ]
+      } ,
+      {
+        numb: 8,
+        question: "There are __ total players on the basketball court at one time.?",
+        answer: "10 players",
+        options: [
+          "15 players",
+          " 5 players",
+          "10 players",
+          "8 players"
+        ]
+      }  
+
+
+
+  ];
